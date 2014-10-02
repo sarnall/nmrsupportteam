3707b65836fba5857fd6839ecfe9c93b31483b54
sarnall <sophie.arnall@uwa.edu.au>
2014-10-02 14:28:36 +0800

    progress!

--- junk2.R
+++ junk2.R
@@ -2,3 +2,5 @@ this is a test code for the nichemappR support group, dedicated to deciphering t
 Yay. 
 Himali is very frustrated now.
 If it works again, I will be happy.
+She's so happy!!
+if (Himali = happy,everyonehappy,everyonesad)
